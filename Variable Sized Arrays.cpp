2dbd103579d0845b1eed1e59ff0c9850045f2761
Abdullah Alakbarli <154253374+abdullahalakbarli@users.noreply.github.com>
2024-10-31 17:56:36 +0400

    Create Variable Sized Arrays.cpp

---
+++ Variable Sized Arrays.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+2 2
+3 1 5 4
+5 1 2 8 9 3
+0 1
+1 3
+*/
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n, q;
+    cin >> n >> q;
+    vector <vector <int>> all_data;
+    for (int i = 0; i < n; i++){
+        int k;
+        cin >> k;
+        vector <int> lst = {};
+        for (int j = 0; j < k; j++){
+            int v = 0;
+            cin >> v;
+            lst.push_back(v);
+        }
+        all_data.push_back(lst);
+    }
+    for (int i = 0; i < q; i++){
+        int q1, q2;
+        cin >> q1 >> q2;
+        cout << all_data[q1][q2] << '\n';
+    }
+    return 0;
+}
